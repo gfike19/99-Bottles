9f5a0138896377e06d6d7503815a7f8e16158132
gfike19 <gfike19@gmail.com>
2020-06-03 23:45:59 -0500

    first test passed.

--- src/com/gfike/Main/Bottle.java
+++ src/com/gfike/Main/Bottle.java
@@ -2,10 +2,15 @@ package com.gfike.Main;
 
 public class Bottle {
     public String verse(int i) {
-        return "";
+        return "99 bottles of beer on the wall, 99 bottles of beer." +
+        "\nTake one down and pass it around, 98 bottles of beer on the wall.";
     }
 
     public String verses(int i, int i1) {
+        return "";
+    }
+
+    public String song() {
         return "";
     }
 }
